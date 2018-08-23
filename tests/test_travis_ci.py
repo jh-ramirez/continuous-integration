6ea2f60c983b8c436eaf7d43c0228ce720a00764
John Ramirez <jhramirez0@gmail.com>
2018-08-22 20:38:19 -0400

    created new unit test

--- tests/test_travis_ci.py
+++ tests/test_travis_ci.py
@@ -6,3 +6,8 @@ class TestModule():
         expected_value = 'Hello World'
         test_value = statement('Hello World')
         assert test_value == expected_value
+
+    def test_statement_alternative(self):
+        expected_value = 'Hello World Two'
+        test_value = statement('Hello World Two')
+        assert test_value == expected_value
